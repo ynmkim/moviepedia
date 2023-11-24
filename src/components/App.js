9405d640e710599bfd082bc9d289259a56971a5a
김윤미 <younmeet@gmail.com>
2023-11-24 15:07:20 +0900

    feat : ReviewList 컴포넌트의 item 정렬 기능 구현

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,19 @@
 import ReviewList from './ReviewList'
 import items from '../mock.json'
+import { useState } from 'react'
 
 function App() {
+  const [order, setOrder] = useState('createdAt')
+  const sortedItem = items.sort((a, b) => b[order] - a[order])
+
+  const handleNewestClick = () => setOrder('createdAt')
+  const handleRatingClick = () => setOrder('rating')
+
   return (
     <div>
-      <ReviewList items={items} />
+      <button onClick={handleNewestClick}>최신순</button>
+      <button onClick={handleRatingClick}>평점순</button>
+      <ReviewList items={sortedItem} />
     </div>
   )
 }
