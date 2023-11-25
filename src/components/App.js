17424b5f862cc97790b54df565126ce33080db6a
김윤미 <younmeet@gmail.com>
2023-11-25 11:26:27 +0900

    feat : ReviewList 컴포넌트의 item 삭제 기능 구현

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,24 @@
 import ReviewList from './ReviewList'
-import items from '../mock.json'
+import mockItems from '../mock.json'
 import { useState } from 'react'
 
 function App() {
+  const [items, setItems] = useState(mockItems)
   const [order, setOrder] = useState('createdAt')
   const sortedItem = items.sort((a, b) => b[order] - a[order])
 
   const handleNewestClick = () => setOrder('createdAt')
   const handleRatingClick = () => setOrder('rating')
+  const handleDelete = (id) => {
+    const nextItems = items.filter((item) => item.id !== id) // true가 되는 요소만 모아서 새로운 배열을 리턴함.
+    setItems(nextItems)
+  }
 
   return (
     <div>
       <button onClick={handleNewestClick}>최신순</button>
       <button onClick={handleRatingClick}>평점순</button>
-      <ReviewList items={sortedItem} />
+      <ReviewList items={sortedItem} onDelete={handleDelete} />
     </div>
   )
 }
--- src/components/ReviewList.js
+++ src/components/ReviewList.js
@@ -5,7 +5,9 @@ function formateDate(value) {
   return `${date.getFullYear()}. ${date.getMonth() + 1}. ${date.getDate()}`
 }
 
-function ReviewListItem({ item }) {
+function ReviewListItem({ item, onDelete }) {
+  const handleDeleteClick = () => onDelete(item.id)
+
   return (
     <div className="ReviewListItem">
       <img className="ReviewListItem-img" src={item.imgUrl} alt={item.title} />
@@ -14,18 +16,19 @@ function ReviewListItem({ item }) {
         <p>{item.rating}</p>
         <p>{formateDate(item.createdAt)}</p>
         <p>{item.content}</p>
+        <button onClick={handleDeleteClick}>삭제</button>
       </div>
     </div>
   )
 }
 
-function ReviewList({ items }) {
+function ReviewList({ items, onDelete }) {
   return (
     <ul>
-      {items.map((item, key) => {
+      {items.map((item) => {
         return (
-          <li key={key}>
-            <ReviewListItem item={item} />
+          <li key={item.id}>
+            <ReviewListItem item={item} onDelete={onDelete} />
           </li>
         )
       })}
