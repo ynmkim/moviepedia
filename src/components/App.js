2e55868506a8b0b40ea4bfd5b1674819bbbe1459
김윤미 <younmeet@gmail.com>
2023-11-26 14:55:45 +0900

    feat : reviews 데이터 가져오기

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,9 @@
 import ReviewList from './ReviewList'
-import mockItems from '../mock.json'
+import { getReviews } from '../api'
 import { useState } from 'react'
 
 function App() {
-  const [items, setItems] = useState(mockItems)
+  const [items, setItems] = useState([])
   const [order, setOrder] = useState('createdAt')
   const sortedItem = items.sort((a, b) => b[order] - a[order])
 
@@ -13,12 +13,17 @@ function App() {
     const nextItems = items.filter((item) => item.id !== id) // true가 되는 요소만 모아서 새로운 배열을 리턴함.
     setItems(nextItems)
   }
+  const handlLoadClick = async () => {
+    const { reviews } = await getReviews()
+    setItems(reviews)
+  }
 
   return (
     <div>
       <button onClick={handleNewestClick}>최신순</button>
       <button onClick={handleRatingClick}>평점순</button>
       <ReviewList items={sortedItem} onDelete={handleDelete} />
+      <button onClick={handlLoadClick}>불러오기</button>
     </div>
   )
 }
